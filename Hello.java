87611e9b11a62c8e916a4b3959acba92b710550b
Darshil Patel <123014945+tun43429@users.noreply.github.com>
2023-09-08 03:03:23 +0000

    CIS 3296 first commit

---
+++ Hello.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.printf("Hello and welcome!");
+        System.out.printf("Hello again!");
+        System.out.printf("Welcome to CIS 3269: Software Design!!");
+         System.out.printf("Hope you have a great semester!");
+
+    }
+}
